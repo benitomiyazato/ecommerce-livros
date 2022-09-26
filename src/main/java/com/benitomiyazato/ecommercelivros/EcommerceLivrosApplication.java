28b85daf99f49061c8e0bfccf5a7236a9dee3fb0
bemiyaz <benito.miya172@gmail.com>
2022-09-26 15:13:08 -0300

    new project

---
+++ src/main/java/com/benitomiyazato/ecommercelivros/EcommerceLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.benitomiyazato.ecommercelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benitomiyazato/ecommercelivros/EcommerceLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benitomiyazato.ecommercelivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
