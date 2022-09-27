f78f29102f9d8d094bc95349b0adba84753b6e0b
bemiyaz <benito.miya172@gmail.com>
2022-09-27 17:44:29 -0300

    book list page

--- src/main/java/com/benitomiyazato/ecommercelivros/controller/AdminController.java
+++ src/main/java/com/benitomiyazato/ecommercelivros/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.benitomiyazato.ecommercelivros.controller;
 
+import com.benitomiyazato.ecommercelivros.model.Book;
+import com.benitomiyazato.ecommercelivros.model.Gender;
 import com.benitomiyazato.ecommercelivros.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/book")
-public class BookController {
+@RequestMapping("/admin")
+public class AdminController {
 
     @Autowired
     private BookService bookService;
 
     @GetMapping
-    public ModelAndView fetchBookList(){
+    public ModelAndView index(){
         ModelAndView mv = new ModelAndView("admin/index");
-        mv.addObject("bookList", bookService.fetchBookList());
+        return mv;
+    }
+
+    @GetMapping("/book")
+    public ModelAndView fetchAllBooks(){
+        ModelAndView mv = new ModelAndView("/admin/book/list");
+        List<Book> bookList = bookService.fetchBookList();
+        mv.addObject("bookList", bookList);
         return mv;
     }
 }
--- src/main/java/com/benitomiyazato/ecommercelivros/service/BookService.java
+++ src/main/java/com/benitomiyazato/ecommercelivros/service/BookService.java
@@ -1,6 +1,7 @@
 package com.benitomiyazato.ecommercelivros.service;
 
 import com.benitomiyazato.ecommercelivros.model.Book;
+import com.benitomiyazato.ecommercelivros.model.Gender;
 import com.benitomiyazato.ecommercelivros.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class BookService {
 
     public List<Book> fetchBookList() {
         return bookRepository.findAll();
+    }
+
+    public Gender getMainGender(Long id){
+        return bookRepository.findById(id).get().getGenders().get(0);
     }
 }
