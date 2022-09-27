c44def104d41d3d292c6417d5f3364d070f73a88
bemiyaz <benito.miya172@gmail.com>
2022-09-27 16:22:31 -0300

    Book Controller GetMapping

---
+++ src/main/java/com/benitomiyazato/ecommercelivros/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.benitomiyazato.ecommercelivros.controller;
+
+import com.benitomiyazato.ecommercelivros.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public ModelAndView fetchBookList(){
+        ModelAndView mv = new ModelAndView("book/listing");
+        mv.addObject("bookList", bookService.fetchBookList());
+        return mv;
+    }
+}
--- src/main/java/com/benitomiyazato/ecommercelivros/service/BookService.java
+++ src/main/java/com/benitomiyazato/ecommercelivros/service/BookService.java
@@ -1,7 +1,19 @@
 package com.benitomiyazato.ecommercelivros.service;
 
+import com.benitomiyazato.ecommercelivros.model.Book;
+import com.benitomiyazato.ecommercelivros.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> fetchBookList() {
+        return bookRepository.findAll();
+    }
 }
